ce014b27e5434a6c644c739ca1e7eb4b2efe6e8e
René Ruppert <rene.ruppert@gmail.com>
2020-08-03 12:01:07 +0200

    Fix script names

--- PostDeployment.ps1
+++ PostDeployment.ps1
@@ -1,3 +1,5 @@
+# Call the script like this:
+# powershell -ExecutionPolicy bypass -File ./PostDeployment.ps1 -repo https://raw.githubusercontent.com/kphillpotts/CloudStudio/master
 param($repo)
 
 #Changing ExecutionPolicy
@@ -5,8 +7,19 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 #Change security protocol
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 
+Set-Location ~/Desktop
+New-Item TempCloudStudio -type directory
 
-$repoUrl = "$repo/InstallStreamingTools.ps1"
-Write-Host "  Downloading installation script from $repoUrl"
-Invoke-WebRequest -Uri $repoUrl -OutFile "~/Desktop/InstallStreamingTools.ps1" -UseBasicParsing
+# Download the script, which installs all the tooling, to the desktop.
+$repoUrl = "$repo/CloudStudioInstallScript.ps1"
+Write-Host "  Downloading CloudStudio installation script from $repoUrl"
+Invoke-WebRequest -Uri $repoUrl -OutFile "~/Desktop/TempCloudStudio/CloudStudioInstallScript.ps1" -UseBasicParsing
+
+# Create a CMD file users can execute to run the script. That's because PS1 files cannot be executed directly.
+# By putting a CMD in front of it, we can make this a one-click-experience.
+# Create an empty file.
+New-Item ./InstallStreamingTools.cmd -ItemType file
+# Write the PowerShell execution command into it.
+Set-Content ./InstallStreamingTools.cmd 'powershell -noprofile -executionpolicy bypass -file ./TempCloudStudio/CloudStudioInstallScript.ps1'
+
 Write-Host "  Done."
